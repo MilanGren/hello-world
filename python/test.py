c37004822c6f2e4e89b519151dcccb9b0c8c28cd
milan gren <gre@ujv.cz>
2017-11-06 15:47:07 +0100

    python

---
+++ python/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import platform
+
+print("%-s %-s" % ("verze je",platform.python_version()))
+
+ary = ['orange', 'apple', 'pear', 'banana', 'kiwi']
+
+print(ary.index('kiwi'))
+
+items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for item in items: #enumerate(items):
+  print(item)
+
+nove = [1,2,None,3,0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i,item in enumerate(items):
+  nove[i] = item
+      
+print(nove)
